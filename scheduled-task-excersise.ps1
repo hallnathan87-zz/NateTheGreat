166b43bdfdda0aaeb2cd3e95882c05c4000958bb
Hall <Hall.Nathan@principal.com>
2019-11-08 10:42:24 -0600

    because

--- scheduled-task-excersise.ps1
+++ scheduled-task-excersise.ps1
@@ -1,4 +1,4 @@
-$Trigger= New-ScheduledTaskTrigger -At 10:16am -Daily # Specify the trigger settings
-$User= "NT AUTHORITY\SYSTEM" # Specify the account to run the script
-$Action= New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument "C:\Git\NateTheGreat\task-to-run.ps1" # Specify what program to run and with its parameters
+$Trigger= New-ScheduledTaskTrigger -At 10:26am -Daily # Specify the trigger settings
+# $User= "NT AUTHORITY\SYSTEM" # Specify the account to run the script
+$Action= New-ScheduledTaskAction -Execute "powershell.exe" #-Argument "C:\Git\NateTheGreat\task-to-run.ps1" # Specify what program to run and with its parameters
 Register-ScheduledTask -TaskName "ExecutingPowerShell" -Trigger $Trigger -Action $Action -RunLevel Highest -Force # Specify the name of the task
